585870960c1815eb541a577f2e6678123c7a4a5d
jbedia <joaquin.bedia@gmail.com>
2014-04-28 11:36:10 +0200

    Internal function to compute the time bounds of each verification time

---
+++ R/timeBounds.R
@@ -0,0 +1,16 @@
+# Internal function to compute the time bounds of each verification time
+timeBounds <- function(dic, foreDates) { 
+      if (!is.null(dic)) {
+            ltb <- as.difftime(dic$lower_time_bound, format = "%H", units = "hours")
+            foreDates <- foreDates - ltb
+            foreDates <- as.POSIXlt(foreDates)
+            varTimeStep <- strptime(dic$time_step, format = "%Hh")$hour
+            foreDatesEnd <- foreDates + varTimeStep*3600
+      } else {
+            varTimeStep <- difftime(foreDates[2], foreDates[1])
+            foreDatesEnd <- foreDates + varTimeStep
+      }
+      foreDatesEnd <- as.POSIXlt(foreDatesEnd)
+      return(list("start" = foreDates, "end" = foreDatesEnd))
+}
+# End
