4b8906d69e24e1f27bafe0ce303de0d8c6a53265
jbedia <joaquin.bedia@gmail.com>
2014-05-30 17:08:06 +0200

    New commented header R-oxygen style

--- R/dictionaryLookup.R
+++ R/dictionaryLookup.R
@@ -1,5 +1,16 @@
-dictionaryLookup <- function(dictionary, var) {
-      dictionary <- tryCatch({read.csv(dictionary, stringsAsFactors = FALSE)}, error = function(e) stop("Dictionary not found"))
+#' Searches variable string in the dictionary
+#' 
+#' Searches variable string provided in the dictionary to map it in the vocabulary, in order to get
+#' all the necessary information for variable homogenization.
+#' 
+#' @param dicPath Full path to the dictionary file (a csv file with extension \sQuote{.dic}).
+#' @param var Character string with the (standard) name of the variable
+#' @return A data.frame of 1 row with the mapping information
+#' @references \url{http://meteo.unican.es/ecoms-udg/RPackage/Homogeneization}
+#' @author J. Bedia \email{joaquin.bedia@@gmail.com}
+
+dictionaryLookup <- function(dicPath, var) {
+      dictionary <- tryCatch({read.csv(dicPath, stringsAsFactors = FALSE)}, error = function(e) stop("Dictionary not found"))
       dicRow <- grep(paste("^", var, "$", sep = ""), dictionary$identifier)
       if (length(dicRow) == 0) {
             stop("Variable requested does not match any identifier in the dictionary")
