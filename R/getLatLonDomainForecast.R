ed4a20f68a119d9419fc742b3d5fd63444e5a90b
jbedia <joaquin.bedia@gmail.com>
2014-06-04 12:22:22 +0200

    Yet another bug fix in point selection mode

--- R/getLatLonDomainForecast.R
+++ R/getLatLonDomainForecast.R
@@ -1,3 +1,31 @@
+#' Define spatial domain of the query
+#' 
+#' @param grid a java \sQuote{GeoGrid}
+#' @param lonLim longitudinal range definition (see \code{\link{loadSeasonalForecast}})
+#' @param latLim latitudinal range definition (see \code{\link{loadSeasonalForecast}})
+#' @return A list of parameters passed to the loading function:
+#' \begin{itemize}{}
+#' \item{llRanges}{A list of length 2 or 1 (selection crosses or not dateline) of 
+#' java \sQuote{LatLonRect} class elements}
+#' \item{pointXYindex}{A vector of length 2 with the index positions for the xy
+#'  coordinates in case of single point selections. See details}
+#' \item{xyCoords}{A list of x and y coordinates (in ascending order)}
+#' \item{revLat}{Logical. Should latitude values be reversed? Sometimes needed for having latitudes
+#'  in ascending order for consistent mapping}
+#' \end{itemize}
+#' @details In order to deal with the problem of dateline crossing, the selection is
+#' partitioned into two, and the part of the domain with negative eastings is put in first
+#' place for consistent spatial mapping.
+#' The index position of lon and lat in the corresponding axes is returned 
+#' by \code{pointXYindex}, and is passed to the \sQuote{readDataSlice} method in
+#'  \code{\link{makeSubset.S4} and \code{\link{makeSubset.CFS}. For single point locations, 
+#'  this is a integer vector of length two defining these positions, while in the case of rectangular
+#'   domains its value is c(-1L,-1L).
+#' @note The function assumes that datasets have degrees-east and degress-north as units
+#' of the corresponding X and Y axes.
+#' @references \url{https://www.unidata.ucar.edu/software/thredds/current/netcdf-java/v4.0/javadocAll/ucar/nc2/dt/GridCoordSystem.html#getRangesFromLatLonRect%28ucar.unidata.geoloc.LatLonRect%29}
+#' @author J. Bedia \email{joaquin.bedia@@gmail.com} and A. Cofin\~no
+
 getLatLonDomainForecast <- function(grid, lonLim, latLim) {
       gcs <- grid$getCoordinateSystem()
       bboxDataset <- gcs$getLatLonBoundingBox()
@@ -6,10 +34,19 @@ getLatLonDomainForecast <- function(grid, lonLim, latLim) {
             if (any(lonLim < 0) & bboxDataset$getLonMin() >= 0) {
                   lon.aux <- sort(lonLim[which(lonLim < 0)] + 360)
             } else {
-                  lon.aux <- lonLim
+                  if (!is.null(lonLim)) {
+                        lon.aux <- lonLim
+                  } else {
+                        lon.aux <- bboxDataset$getCenterLon()
+                  }
             }
             if (length(lonLim) == 1) {
-                  pointXYindex[1] <- gcs$findXYindexFromCoord(lon.aux, latLim[1], .jnull())[1]
+                  if (is.null(latLim)) {
+                        lat.aux <- bboxDataset$getLatMin()
+                  } else {
+                        lat.aux <- latLim[1]
+                  }
+                  pointXYindex[1] <- gcs$findXYindexFromCoord(lon.aux, lat.aux, .jnull())[1]
                   if (pointXYindex[1] < 0) {
                         stop("Selected X point coordinate is out of range")
                   }
@@ -25,8 +62,8 @@ getLatLonDomainForecast <- function(grid, lonLim, latLim) {
       }
       if (is.null(lonLim)) {
             lonLim <- c(bboxDataset$getLonMin(), bboxDataset$getLonMax())
-            if (any(lonLim > 180)) {
-                  lonLim <- lonLim - 180      
+            if (all(pointXYindex < 0)) {
+                  lonLim <- lonLim - 180
             }
       }
       if (is.null(latLim)) {
@@ -61,7 +98,6 @@ getLatLonDomainForecast <- function(grid, lonLim, latLim) {
             }
             lonSlice <- do.call("c", lonAux)      
       }
-      
       lonSlice[which(lonSlice > 180)] <- lonSlice[which(lonSlice > 180)] - 360
       lonSlice <- sort(lonSlice)
       aux <- grid$makeSubset(.jnull(), .jnull(), .jnull(), .jnull(), llRanges[[1]]$get(0L), llRanges[[1]]$get(1L))
@@ -82,4 +118,3 @@ getLatLonDomainForecast <- function(grid, lonLim, latLim) {
       return(list("llRanges" = llRanges, "pointXYindex" = pointXYindex, "xyCoords" = list("x" = lonSlice, "y" = latSlice), "revLat" = revLat))            
 }
 # End
-
