8848ce3e15ce242927eba376c0cdbe013a82287a
jbedia <joaquin.bedia@gmail.com>
2014-06-08 12:14:26 +0200

    Modified to account for 6h to daily mean aggregations

--- R/timeBounds.R
+++ R/timeBounds.R
@@ -1,8 +1,14 @@
 # Internal function to compute the time bounds of each verification time
 timeBounds <- function(dic, foreDates) { 
       if (!is.null(dic)) {
-            ltb <- as.difftime(dic$lower_time_bound, format = "%H", units = "hours")
-            utb <- as.difftime(dic$upper_time_bound, format = "%H", units = "hours")
+            if (isTRUE(dic$doDailyMean)) {
+                  foreDates <- foreDates[seq.int(1, length(foreDates), 4)]
+                  ltb <- as.difftime(0, format = "%H", units = "hours")
+                  utb <- as.difftime(24, format = "%H", units = "hours")
+            } else {
+                  ltb <- as.difftime(dic$lower_time_bound, format = "%H", units = "hours")
+                  utb <- as.difftime(dic$upper_time_bound, format = "%H", units = "hours")
+            }
             foreDates <- as.POSIXlt(foreDates - ltb)
             foreDatesEnd <- as.POSIXlt(foreDates + utb)
       } else {
