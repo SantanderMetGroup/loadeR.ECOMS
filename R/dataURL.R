27ee1088c897fce9b676902734ad7d5a06057617
jbedia <joaquin.bedia@gmail.com>
2014-06-11 14:44:38 +0200

    Alpha S4 dataset moved to its original URL

--- R/dataURL.R
+++ R/dataURL.R
@@ -12,7 +12,7 @@
 dataURL <- function(dataset) {
       dataset <- match.arg(dataset, c("System4_seasonal_15", "System4_seasonal_51", "System4_annual_15", "CFSv2_seasonal_16"))
       switch(dataset, 
-            "System4_seasonal_15" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/system4/System4_Seasonal_15Members_alpha.ncml", "excludeVars" = NULL),
+            "System4_seasonal_15" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/system4/System4_Seasonal_15Members.ncml", "excludeVars" = NULL),
             "System4_seasonal_51" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/system4/System4_Seasonal_51Members.ncml", "excludeVars" = NULL),
             "System4_annual_15" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/system4/System4_Annual_15Members.ncml", "excludeVars" = NULL),
             "CFSv2_seasonal_16" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/cfs/agg/cfsAgg_fmrc.ncd", "excludeVars" = "Pressure_reduced_to_MSL_msl"))
