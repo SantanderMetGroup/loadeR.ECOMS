02ffba29ee4927bc62e1070cd946ff353ab0f7fb
jbedia <joaquin.bedia@gmail.com>
2014-06-11 14:48:37 +0200

    Parts of the code moved to sub-functions
    findPointXYindex
    getXYcoordinatesForecast

--- R/getLatLonDomainForecast.R
+++ R/getLatLonDomainForecast.R
@@ -27,38 +27,16 @@
 #' @author J. Bedia \email{joaquin.bedia@@gmail.com} and A. Cofin\~no
 
 getLatLonDomainForecast <- function(grid, lonLim, latLim) {
+      message("[", Sys.time(), "] Defining geo-location parameters")
       gcs <- grid$getCoordinateSystem()
       bboxDataset <- gcs$getLatLonBoundingBox()
-      pointXYindex <- c(-1L, -1L)
       if (length(lonLim) == 1 | length(latLim) == 1) {
-            if (any(lonLim < 0) & bboxDataset$getLonMin() >= 0) {
-                  lon.aux <- sort(lonLim[which(lonLim < 0)] + 360)
-            } else {
-                  if (!is.null(lonLim)) {
-                        lon.aux <- lonLim
-                  } else {
-                        lon.aux <- bboxDataset$getCenterLon()
-                  }
-            }
-            if (length(lonLim) == 1) {
-                  if (is.null(latLim)) {
-                        lat.aux <- bboxDataset$getLatMin()
-                  } else {
-                        lat.aux <- latLim[1]
-                  }
-                  pointXYindex[1] <- gcs$findXYindexFromCoord(lon.aux, lat.aux, .jnull())[1]
-                  if (pointXYindex[1] < 0) {
-                        stop("Selected X point coordinate is out of range")
-                  }
-                  lonLim <- NULL   
-            }
-            if (length(latLim) == 1) {
-                  pointXYindex[2] <- gcs$findXYindexFromCoord(lon.aux[1], latLim, .jnull())[2]
-                  if (pointXYindex[2] < 0) {
-                        stop("Selected Y point coordinate is out of range")
-                  }
-                  latLim <- NULL
-            }
+            pointXYpars <- findPointXYindex(lonLim, latLim, gcs)
+            lonLim <- pointXYpars$lonLim
+            latLim <- pointXYpars$latLim
+            pointXYindex <- pointXYpars$pointXYindex
+      } else {
+            pointXYindex <- c(-1L, -1L)
       }
       if (is.null(lonLim)) {
             lonLim <- c(bboxDataset$getLonMin(), bboxDataset$getLonMax())
@@ -83,38 +61,7 @@ getLatLonDomainForecast <- function(grid, lonLim, latLim) {
             llRanges[[1]] <- gcs$getRangesFromLatLonRect(.jnew("ucar/unidata/geoloc/LatLonRect", spec))
       }
       # Coordinates
-      if (pointXYindex[1] >= 0) {
-            aux <- grid$makeSubset(.jnull(), .jnull(), .jnull(), 1L, 1L, 1L)
-            lonSlice <- aux$getCoordinateSystem()$getLonAxis()$getCoordValue(pointXYindex[1])
-      } else {
-            lonAux <- list()
-            for (k in 1:length(llRanges)) {
-                  aux <- grid$makeSubset(.jnull(), .jnull(), .jnull(), .jnull(), llRanges[[k]]$get(0L), llRanges[[k]]$get(1L))
-                  lonAxisShape <- aux$getCoordinateSystem()$getXHorizAxis()$getRank()
-                  lonAux[[k]] <- aux$getCoordinateSystem()$getXHorizAxis()$getCoordValues()
-                  if (lonAxisShape > 1) {
-                        lonAux[[k]] <- apply(t(matrix(lonAux[[k]], ncol = lonAxisShape[1])), 2, min)
-                  } 
-            }
-            lonSlice <- do.call("c", lonAux)      
-      }
-      lonSlice[which(lonSlice > 180)] <- lonSlice[which(lonSlice > 180)] - 360
-      lonSlice <- sort(lonSlice)
-      aux <- grid$makeSubset(.jnull(), .jnull(), .jnull(), .jnull(), llRanges[[1]]$get(0L), llRanges[[1]]$get(1L))
-      revLat <- FALSE
-      if (pointXYindex[2] >= 0) {
-            latSlice <- aux$getCoordinateSystem()$getYHorizAxis()$getCoordValue(pointXYindex[2])
-      } else {
-            latSlice <- aux$getCoordinateSystem()$getYHorizAxis()$getCoordValues()
-            latAxisShape <- aux$getCoordinateSystem()$getYHorizAxis()$getRank()
-            if (latAxisShape > 1) {
-                  latSlice <- apply(t(matrix(latSlice, ncol = latAxisShape[1])), 1, min)
-            }
-            if (diff(latSlice)[1] < 0) {
-                  latSlice <- rev(latSlice)
-                  revLat <- TRUE
-            }
-      }
-      return(list("llRanges" = llRanges, "pointXYindex" = pointXYindex, "xyCoords" = list("x" = lonSlice, "y" = latSlice), "revLat" = revLat))            
+      coordPars <- getXYcoordinatesForecast(grid, pointXYindex, llRanges)
+      return(list("llRanges" = llRanges, "pointXYindex" = pointXYindex, "xyCoords" = list("x" = coordPars$x, "y" = coordPars$y), "revLat" = coordPars$revLat))            
 }
 # End
