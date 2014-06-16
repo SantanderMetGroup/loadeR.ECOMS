8f5d0bea69eadef76ed5e113a33330ab63475011
jbedia <joaquin.bedia@gmail.com>
2014-06-16 14:24:29 +0200

    startup message removed

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,6 +1,5 @@
 .onLoad <- function(libname, pkgname = "netcdfAll-4.3.jar") {
       .jpackage(pkgname, lib.loc = libname)
       options(java.parameters = "-Xmx2g")
-      cat("ecomsUDG.Raccess version 2.0-0 (2014-06-16) is loaded")
 }
 
