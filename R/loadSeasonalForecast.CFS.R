ba070a749a0542f069ceff19c0f5d2e63ce9255a
jbedia <joaquin.bedia@gmail.com>
2014-06-16 14:26:28 +0200

    Minor bug fix and other changes
    Minor bug fix: transformation is only undertaken if dic is not null
    
    ForecastDates are now Dates, for compatibility with (non-forecast) gridded datasets

--- R/loadSeasonalForecast.CFS.R
+++ R/loadSeasonalForecast.CFS.R
@@ -2,18 +2,19 @@ loadSeasonalForecast.CFS = function(var, grid, dic, latLon, runTimePars, time) {
       gcs <- grid$getCoordinateSystem()
       foreTimePars <- getForecastTimeDomain.CFS(grid, dic, runTimePars, time)
       mdArray <- makeSubset.CFS(grid, latLon, runTimePars, foreTimePars)
-      mdArray <- dictionaryTransformForecast(dic, foreTimePars, mdArray)
+      if (!is.null(dic)) {
+            isStandard <- TRUE
+            mdArray <- dictionaryTransformForecast(dic, foreTimePars, mdArray)
+      } else {
+            isStandard <- FALSE
+      }
       if (isTRUE(latLon$revLat)) {
             mdArray <- revArrayLatDim(mdArray, grid)
-      }
-      isStandard <- TRUE
-      if (is.null(dic)) {
-            isStandard <- FALSE
       }
       return(list("Variable" = list("varName" = var, "isStandard" = isStandard),
                   "Data" = mdArray,
                   "xyCoords" = c(latLon$xyCoords, "CRS_string" = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"), 
-                  "ForecastDates" = foreTimePars$forecastDates,
+                  "Dates" = foreTimePars$forecastDates,
                   "InitializationDates" = runTimePars$runDates))
 }
 # End
--- R/loadSeasonalForecast.S4.R
+++ R/loadSeasonalForecast.S4.R
@@ -20,18 +20,19 @@ loadSeasonalForecast.S4 <- function(dataset, var, grid, dic, members, latLon, ru
       memberRangeList <- getMemberDomain.S4(grid, dataset, members)
       foreTimePars <- getForecastTimeDomain.S4(grid, dataset, dic, runTimePars, time)
       mdArray <- makeSubset.S4(grid, latLon, runTimePars, memberRangeList, foreTimePars)
-      mdArray <- dictionaryTransformForecast(dic, foreTimePars, mdArray)
+      if (!is.null(dic)) {
+            isStandard <- TRUE
+            mdArray <- dictionaryTransformForecast(dic, foreTimePars, mdArray)
+      } else {
+            isStandard <- FALSE
+      }
       if (isTRUE(latLon$revLat)) {
             mdArray <- revArrayLatDim(mdArray, grid)
-      }
-      isStandard <- TRUE
-      if (is.null(dic)) {
-            isStandard <- FALSE
       }
       return(list("Variable" = list("varName" = var, "isStandard" = isStandard),
                   "Data" = mdArray,
                   "xyCoords" = c(latLon$xyCoords, "CRS_string" = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"), 
-                  "ForecastDates" = foreTimePars$forecastDates,
+                  "Dates" = foreTimePars$forecastDates,
                   "InitializationDates" = runTimePars$runDates))
 }
 # End
