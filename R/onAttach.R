bd166c1caa5d3fbe5e532ad1f405360771e49ef6
jbedia <joaquin.bedia@gmail.com>
2014-06-19 16:04:08 +0200

    Several tests for automatic netcdfJava download onAttach

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,8 +1,16 @@
 .onAttach <- function(...) {
-      mylib <- dirname(system.file(package = "ecomsUDG.Raccess"))
+      pkgname <- "ecomsUDG.Raccess"
+      lib <- system.file(package = pkgname)
       ver <- packageVersion("ecomsUDG.Raccess")
       builddate <- packageDescription("ecomsUDG.Raccess")$Date
-      mess <- paste("ecomsUDG.Raccess version ", ver ," (", builddate,") is loaded", sep = "")                        
+      mess <- paste(pkgname, " version ", ver, " (", builddate,") is loaded", sep = "")                        
       packageStartupMessage(mess)
+#       jar <- "netcdfAll-4.3.jar"
+#       if (!file.exists(file.path(lib, "java", jar))) {
+#             packageStartupMessage("NetCDF-Java not available. Attempting download...")
+#             destdir <- file.path(lib, "java")
+#             dir.create(path = destdir)
+#             download.file("ftp://ftp.unidata.ucar.edu/pub/netcdf-java/v4.3/netcdfAll-4.3.jar", destfile = file.path(destdir, jar))
+#       }
 } 
 # End
