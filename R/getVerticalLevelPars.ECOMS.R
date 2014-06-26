5cbc0b071b7b768d81fe87b684cb368d07909911
jbedia <joaquin.bedia@gmail.com>
2014-06-26 12:34:59 +0200

    Specific error messages for ECOMS referring to the wiki

---
+++ R/getVerticalLevelPars.ECOMS.R
@@ -0,0 +1,32 @@
+#' Definition of vertical dimension slices
+#' 
+#' Returns the selected level value (if any) and a suitable java structure. This is a subroutine
+#' of \code{loadGridDataset}, whose output is passed to \code{makeSubset}. 
+#'  
+#' @param gcs An object of the java class \sQuote{GeoGrid})
+#' @param level Vertical level. Passed by \code{loadGridDataset}, obtained via \code{findVerticalLevel}
+#' @return A list with the level value and either a java Range or a java null reference
+#' defining the index of the vertical axis (null if no vertical levels exist)
+#' @author J. Bedia \email{joaquin.bedia@@gmail.com}
+
+getVerticalLevelPars.ECOMS <- function(grid, level) {
+    gcs <- grid$getCoordinateSystem()
+    if (gcs$hasVerticalAxis()) {
+        if (is.null(level)) {
+            stop("Variable with vertical levels: '@level' following the variable name is required.\nGo to <http://www.meteo.unican.es/ecoms-udg/DataServer/ListOfVariables> for details")
+        }
+        levelInd <- gcs$getVerticalAxis()$findCoordElement(level)
+        if (levelInd < 0) {
+            stop("Vertical level not found.\nGo to <http://www.meteo.unican.es/ecoms-udg/DataServer/ListOfVariables> for valid vertical level values")
+        }
+        zRange <- .jnew("ucar/ma2/Range", levelInd, levelInd)
+    } else {
+        if (!is.null(level)) {
+            warning("The variable selected has no vertical levels")
+        }
+        level <- NULL
+        zRange <- .jnull()    
+    }
+    return(list("level" = level, "zRange" = zRange))
+}
+# End
