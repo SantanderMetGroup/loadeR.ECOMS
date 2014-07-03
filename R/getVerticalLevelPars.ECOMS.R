780f9dbb3e7006ef729ba4677d78473b926abeb4
jbedia <joaquin.bedia@gmail.com>
2014-07-03 13:44:48 +0200

    Modified on screen message and very minor change in code

--- R/getVerticalLevelPars.ECOMS.R
+++ R/getVerticalLevelPars.ECOMS.R
@@ -22,9 +22,9 @@ getVerticalLevelPars.ECOMS <- function(grid, level) {
         zRange <- .jnew("ucar/ma2/Range", levelInd, levelInd)
     } else {
         if (!is.null(level)) {
-            warning("The variable selected has no vertical levels: the '@level' specification was ignored")
+            warning("The variable selected is 2D: the '@level' specification was ignored")
+            level <- NULL
         }
-        level <- NULL
         zRange <- .jnull()    
     }
     return(list("level" = level, "zRange" = zRange))
