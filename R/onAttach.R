f8a6f808e89126be798a9451506c01bc62d24099
jbedia <joaquin.bedia@gmail.com>
2014-07-16 11:43:14 +0200

    catch error in case there is no internet connection on attach

--- R/onAttach.R
+++ R/onAttach.R
@@ -6,14 +6,19 @@
       mess <- paste(pkgname, " version ", ver, " (", builddate,") is loaded", sep = "")                        
       packageStartupMessage(mess)
       url <- "https://raw.githubusercontent.com/SantanderMetGroup/ecomsUDG.Raccess/stable/DESCRIPTION"
-      a <- getURL(url)
-      b <- readLines(textConnection(a))
-      latest.ver <- package_version(gsub("Version: ", "", b[grep("Version", b)]))
-      if (ver < latest.ver) {
-            ver.mess1 <- "WARNING: Your current version of ecomsUDG.Raccess is not up-to-date"
-            ver.mess <- paste("Get the latest stable version", latest.ver, "at http://meteo.unican.es/ecoms-udg/RPackage#Versions")      
-            packageStartupMessage(ver.mess1)
-            packageStartupMessage(ver.mess)
+      con <- tryCatch(getURL(url, ssl.verifypeer = FALSE), error = function(er) {
+            er <- NULL
+            return(er)
+      })
+      if (!is.null(con)) {
+            b <- readLines(textConnection(con))
+            latest.ver <- package_version(gsub("Version: ", "", b[grep("Version", b)]))
+            if (ver < latest.ver) {
+                  ver.mess1 <- "WARNING: Your current version of ecomsUDG.Raccess is not up-to-date"
+                  ver.mess <- paste("Get the latest stable version", latest.ver, "at http://meteo.unican.es/ecoms-udg/RPackage#Versions")      
+                  packageStartupMessage(ver.mess1)
+                  packageStartupMessage(ver.mess)
+            }
       }
 } 
 # End
