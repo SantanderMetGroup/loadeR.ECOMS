33cb03b180595303bd89ef7c5fdab6470e69414a
jbedia <joaquin.bedia@gmail.com>
2015-04-29 17:58:30 +0200

    Modified onAttach file
    Points to new stable branch set to 'master'

--- R/onAttach.R
+++ R/onAttach.R
@@ -5,7 +5,7 @@
       builddate <- packageDescription(pkgname)$Date
       mess <- paste(pkgname, " version ", ver, " (", builddate,") is loaded", sep = "")                        
       packageStartupMessage(mess)
-      url <- "https://raw.githubusercontent.com/SantanderMetGroup/ecomsUDG.Raccess/stable/DESCRIPTION"
+      url <- "https://raw.githubusercontent.com/SantanderMetGroup/ecomsUDG.Raccess/master/DESCRIPTION"
       con <- tryCatch(getURL(url, ssl.verifypeer = FALSE), error = function(er) {
             er <- NULL
             return(er)
@@ -15,7 +15,7 @@
             latest.ver <- package_version(gsub("Version: ", "", b[grep("Version", b)]))
             if (ver < latest.ver) {
                   ver.mess1 <- "WARNING: Your current version of ecomsUDG.Raccess is not up-to-date"
-                  ver.mess <- paste("Get the latest stable version", latest.ver, "at http://meteo.unican.es/ecoms-udg/RPackage#Versions")      
+                  ver.mess <- paste("Get the latest stable version", latest.ver, "at http://meteo.unican.es/trac/wiki/udg/ecoms/RPackage/versions#Versions")      
                   packageStartupMessage(ver.mess1)
                   packageStartupMessage(ver.mess)
             }
