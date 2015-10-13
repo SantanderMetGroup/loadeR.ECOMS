f6c0875518f8d54b4bf0bc2e926b8b7cec29ef89
jbedia <joaquin.bedia@gmail.com>
2015-10-13 15:42:47 +0200

    Login function imported from downscaleR

--- R/loginECOMS_UDG.R
+++ R/loginECOMS_UDG.R
@@ -1,11 +1,4 @@
 loginECOMS_UDG <- function(username, password, proxy.host = NULL, proxy.port = NULL) {
-      proxy.port <- as.integer(proxy.port)
-      if (!is.character(username) | !is.character(password)) {
-            stop("\'username\' and \'password\' must be character strings")
-      }
-      if (!is.null(proxy.host)) {
-            J("ucar.nc2.util.net.HTTPSession")$setGlobalProxy(proxy.host, proxy.port)
-      }
-      J("ucar.httpservices.MyHTTPFactory")$setCredentials(username, password)
+      loginUDG(username, password, proxy.host, proxy.port)
 }
 # End
