8df37e343f972cbc00f048d644992366cd02e2aa
jbedia <joaquin.bedia@gmail.com>
2015-12-02 11:20:41 +0100

    Added new Data URL lookup table
    Replaces former switch in dataURL function by a csv table in inst/dataurl

--- R/dataURL.R
+++ R/dataURL.R
@@ -8,19 +8,26 @@
 #' \item \code{URL}. URL path to the dataset
 #' \item \code{excludeVars}. Character vector or NULL, indicating the variables in the dataset to be ignored
 #' }
-#' @author J. Bedia \email{joaquin.bedia@@gmail.com}
+#' @author J. Bedia 
 #' @keywords internal
 
+# dataURL <- function(dataset) {
+#       x <- switch(dataset, 
+#             "System4_seasonal_15" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/system4/System4_Seasonal_15Members.ncml", "excludeVars" = NULL),
+#             "System4_seasonal_51" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/system4/System4_Seasonal_51Members.ncml", "excludeVars" = NULL),
+#             "System4_annual_15" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/system4/System4_Annual_15Members.ncml", "excludeVars" = NULL),
+#             "CFSv2_seasonal" = list("URL" = "http://meteo.unican.es/tds5/dodsC/cfsrr/CFSv2_Seasonal.ncml", "excludeVars" = NULL),
+#             "SMHI-EC-EARTH_EUPORIAS" = list("URL" = "http://meteo.unican.es/tds5/dodsC/ec_earth_v3/smhi_ec_earth_4xDaily.ncml", "excludeVars" = NULL),
+#             "WFDEI" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/wfdei/wfdei_daily.ncml", "excludeVars" = NULL),
+#             "NCEP_reanalysis1" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/ncepReanalysis1/ncepReanalysis1_4xDaily.ncml", "excludeVars" = NULL),
+#             "ERA_interim" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/interim/daily/interim20_daily.ncml", "excludeVars" = NULL))
+#       return(x)
+# }
+# # End
+
 dataURL <- function(dataset) {
-      x <- switch(dataset, 
-            "System4_seasonal_15" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/system4/System4_Seasonal_15Members.ncml", "excludeVars" = NULL),
-            "System4_seasonal_51" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/system4/System4_Seasonal_51Members.ncml", "excludeVars" = NULL),
-            "System4_annual_15" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/system4/System4_Annual_15Members.ncml", "excludeVars" = NULL),
-            "CFSv2_seasonal" = list("URL" = "http://meteo.unican.es/tds5/dodsC/cfsrr/CFSv2_Seasonal.ncml", "excludeVars" = NULL),
-            "SMHI-EC-EARTH_EUPORIAS" = list("URL" = "http://meteo.unican.es/tds5/dodsC/ec_earth_v3/smhi_ec_earth_4xDaily.ncml", "excludeVars" = NULL),
-            "WFDEI" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/wfdei/wfdei_daily.ncml", "excludeVars" = NULL),
-            "NCEP_reanalysis1" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/ncepReanalysis1/ncepReanalysis1_4xDaily.ncml", "excludeVars" = NULL),
-            "ERA_interim" = list("URL" = "http://www.meteo.unican.es/tds5/dodsC/interim/daily/interim20_daily.ncml", "excludeVars" = NULL))
-      return(x)
+      a <- read.csv(file.path(find.package("ecomsUDG.Raccess"), "dataurl","dataURL.csv"))
+      as.list(a[grep(dataset, a$dataset),2:3])
 }
-# End
+      
+      
