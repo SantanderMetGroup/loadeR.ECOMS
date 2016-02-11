528492a460962e5aba8b8a576bd13819ae0c297d
jbedia <joaquin.bedia@gmail.com>
2016-02-11 13:57:11 +0100

    Included deprecated function for backwards comp

---
+++ R/loginECOMS_UDG.R
@@ -0,0 +1,4 @@
+loginECOMS_UDG <- function(username, password, proxy.host = NULL, proxy.port = NULL) {
+      .Deprecated("loginUDG", package = "loadeR")      
+      loginUDG(username, password, proxy.host, proxy.port)
+}
