3eeebd043ee82975e1c500ccaf93b831968bfec9
jbedia <joaquin.bedia@gmail.com>
2016-02-11 14:39:37 +0100

    Doc update

--- R/loginECOMS_UDG.R
+++ R/loginECOMS_UDG.R
@@ -1,4 +1,5 @@
 loginECOMS_UDG <- function(username, password, proxy.host = NULL, proxy.port = NULL) {
-      .Deprecated("loginUDG", package = "loadeR")      
+      .Deprecated("loginUDG", package = "loadeR", msg = "'loginECOMS_UDG' is deprecated.\nUse 'loginUDG' from package loadeR instead.")      
       loginUDG(username, password, proxy.host, proxy.port)
 }
+
