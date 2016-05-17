3839f03e64628574fad96cf0a5dc13d8ff86c8d1
jbedia <joaquin.bedia@gmail.com>
2016-05-17 15:44:10 +0200

    onAttach typos fixed

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,10 +1,11 @@
 .onAttach <- function(...) {
       pkgname <- "loadeR.ECOMS"
+      lib <- system.file(package = pkgname)
       ver <- packageDescription(pkgname)$Version
       builddate <- packageDescription(pkgname)$Date
-      mess <- paste(pkgname, " version ", ver, " (", builddate,") is loaded", sep = "")                        
+      mess <- paste(pkgname, " version ", ver, " (", builddate,") is loaded", sep = "")
       packageStartupMessage(mess)
-      url <- "https://raw.githubusercontent.com/SantanderMetGroup/loadeR.ECOMS/master/DESCRIPTION"
+      url <- paste0("https://raw.githubusercontent.com/SantanderMetGroup/", pkgname, "/master/DESCRIPTION")
       con <- tryCatch(getURL(url, ssl.verifypeer = FALSE), error = function(er) {
             er <- NULL
             return(er)
@@ -13,12 +14,14 @@
             b <- readLines(textConnection(con))
             latest.ver <- package_version(gsub("Version: ", "", b[grep("Version", b)]))
             if (ver < latest.ver) {
-                  ver.mess1 <- "WARNING: Your current version of loadeR.ECOMS is not up-to-date"
-                  ver.mess <- paste("Get the latest stable version", latest.ver, "typing on your R console:\ndevtools::install_github(\"SantanderMetGroup/loadeR.ECOMS\")")      
+                  ver.mess1 <- paste0("WARNING: Your current version of ", pkgname, " (v", ver, ") is not up-to-date")
+                  ver.mess <- paste0("Get the latest stable version (", latest.ver,
+                                     ") using <devtools::install_github('SantanderMetGroup/", pkgname, "')>")
                   packageStartupMessage(ver.mess1)
                   packageStartupMessage(ver.mess)
             }
       }
-} 
+}
 # End
+
 
